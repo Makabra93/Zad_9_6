7f6e69bd60e5b58880131655ec6455ee3a797a3e
Mateusz Lawczys <ma6tek@gmail.com>
2017-12-13 20:48:22 +0100

    Improve funcionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
-var list = document.getElementById('list');
-var add = document.getElementById('addElem');
-var task = document.getElementById('rev');
+var list  = document.getElementById('list');
+var add   = document.getElementById('addElem');
+var task  = document.getElementById('rev');
 
 add.addEventListener('click', function() {
+    var element     = document.createElement('li');
+    var liElem      = list.children;
+    var nextNumber  = liElem.length;
 
-    var element = document.createElement('li');
-    var liElem = document.getElementsByTagName('li'); 
-    var nextNumber = liElem.length;
     element.innerHTML = 'item ' + nextNumber;
     list.appendChild(element);
-
-  });
+});
 
 task.addEventListener('click', function() {
-
     var header = document.getElementById('he3');
     header.innerText = '9.6!';
 
-  });
+});
 
   
